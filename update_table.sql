09f826a8b2fb7c2072b2c7ef5788a0dfe4f6b80b
Fendi <endytc@gmail.com>
2014-03-15 11:29:02 +0700

    sql

--- update_table.sql
+++ update_table.sql
@@ -1,5 +1,2 @@
-ALTER TABLE  `promo` DROP  `id_fasilitas` ;
-ALTER TABLE  `promo` ADD  `transaksi_min` INT NOT NULL ,
-ADD  `transaksi_max` INT NOT NULL ;
-ALTER TABLE  `promo` CHANGE  `transaksi_min`  `transaksi_min` INT( 11 ) NULL ,
-CHANGE  `transaksi_max`  `transaksi_max` INT( 11 ) NULL ;
+ALTER TABLE  `detail_checkin` ADD  `waktu_mengambil_kunci` TIMESTAMP NULL ,
+ADD  `waktu_mengembalikan_kunci` TIMESTAMP NULL ;

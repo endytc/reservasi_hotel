60947423fc6c66573164bfa535cc3b4c06267c10
Fendi <endytc@gmail.com>
2014-03-19 09:47:02 +0700

    approve

--- app/actions/pageoperator/approve.php
+++ app/actions/pageoperator/approve.php
@@ -4,16 +4,17 @@ $checkIn=  _select_unique_result("select checkin.*,
     (select sum(biaya) from detail_checkin where id_checkin=checkin.id) as jumlah_tagihan
     from checkin where id='$_GET[id]'");
 $dataUpdate=array('status'=>$_GET['status']);
-if($checkIn['jumlah_bayar']>=$checkIn['jumlah_tagihan']){
+//if($checkIn['jumlah_bayar']>=$checkIn['jumlah_tagihan']){
     $minPoin= _select_unique_result("select * from setting where kd='min_poin'");
     $poin   = floor($checkIn['jumlah_tagihan']/$minPoin['isi']);
-}
+//echo $checkIn['jumlah_tagihan'].' '.$minPoin['isi'].' '.$poin;exit;
+//}
 $success=_update('checkin', $dataUpdate, "id='$_GET[id]'");
 if($success){
     if($poin==null || $poin=='')
         $poin=0;
-    _update('pengunjung', array('jumlah_poin'=>$poin), "id=$checkIn[id_pengunjung]");
-    _query("update pengunjung set jumlah_poin=jumlah_poin+$poin where id='$checkIn[id_pengunjung]'");
+    $pengunjung=_select_unique_result("select * from pengunjung where id='$checkIn[id_pengunjung]'");
+    _update('pengunjung', array('jumlah_poin'=>$poin+$pengunjung['jumlah_poin']), "id=$checkIn[id_pengunjung]");
     $_SESSION['success']='Data berhasil diperbarui';
 }else
     $_SESSION['failed']='Data gagal diperbarui';
